e7b1159c671e348035719f0ed5d21e9d25106d6a
zhanghongjie-feel <2120155019@qq.com>
2019-12-11 08:17:05 +0800

    q

---
+++ config/my.php
@@ -0,0 +1,14 @@
+<?php
+$config->installed       = true;
+$config->debug           = false;
+$config->requestType     = 'GET';
+$config->timezone        = 'Asia/Shanghai';
+$config->db->host        = '47.100.211.165';
+$config->db->port        = '3306';
+$config->db->name        = 'zentao';
+$config->db->user        = 'root';
+$config->db->encoding    = 'UTF8';
+$config->db->password    = 'oneinstack';
+$config->db->prefix      = 'zt_';
+$config->webRoot         = getWebRoot();
+$config->default->lang   = 'zh-cn';
